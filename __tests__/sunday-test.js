07e29b9267a63c32d6a3697834cf7038bcfad73d
Brandan Sayarath and Michelle Morin <michelle@>
2020-02-04 15:03:28 -0800

    pass second test

--- __tests__/sunday-test.js
+++ __tests__/sunday-test.js
@@ -3,8 +3,12 @@ import { leapYear } from './../src/leapyear.js';
 describe('leapYear', () => {
 
   test('should determine that a year is not a leap year when the year is not divisible by 4', () => {
-    var year = 1993;
-    var result = leapYear(year);
+    var result = leapYear(1993);
     expect(result).toEqual(false);
+  });
+
+  test('should determine that a year is a leap year when the year is divisible by 4', () => {
+    var result = leapYear(2004);
+    expect(result).toEqual(true);
   });
 });
