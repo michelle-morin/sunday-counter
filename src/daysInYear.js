fed70001af6576a6b770985e2f986b1140400493
Brandan Sayarath and Michelle Morin <brandan@>
2020-02-04 15:18:56 -0800

    add business logic to daysInYear function to pass test

--- src/daysInYear.js
+++ src/daysInYear.js
@@ -1,3 +1,10 @@
-export function daysInYear(year) {
+import { leapYear } from './../src/leapyear.js';
 
+export function daysInYear(year) {
+  var result = leapYear(year);
+  if (result === true) {
+    return 366;
+  } else {
+    return 365;
+  }
 };
