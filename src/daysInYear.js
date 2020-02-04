0d4c8ab757c65b75fe5a7700744d6cc6b2d913aa
Brandan Sayarath and Michelle Morin <brandan@>
2020-02-04 15:45:22 -0800

    add business logic for determining first day of every year between 1901 and 2000

--- src/daysInYear.js
+++ src/daysInYear.js
@@ -10,5 +10,19 @@ export function daysInYear(year) {
 };
 
 export function startDayOfYear(year) {
-  
-}
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var prevYearStart = 1;
+  var firstDaysOfYears = [];
+  for (var i=1901; 1<2001; i++) {
+    if (leapYear(i-1) === false) {
+      var yearStart = days[prevYearStart + 1];
+      firstDaysOfYears.push(yearStart);
+      prevYearStart++;
+    } else if (leapYear(i-1)) {
+      yearStart = days[prevYearStart + 2];
+      firstDaysOfYears.push(yearStart);
+      prevYearStart++;
+    }
+  }
+return firstDaysOfYears[0];
+};
